5c977b262e0419c89e938868063b9ae64b8f2001
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2020-04-06 18:39:26 +0200

    pozadina

--- projekat/src/app/app-routing.module.ts
+++ projekat/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrijavaFormaComponent } from './components/prijava-forma/prijava-forma.component';
+import { RegistracijaFormaComponent } from './components/registracija-forma/registracija-forma.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/pocetna",
+    pathMatch: 'full'
+  },
+  {
+    path: "prijava",
+    component: PrijavaFormaComponent
+  },
+  {
+    path: "registracija",
+    component: RegistracijaFormaComponent
+  },
+  {
+    path: "pocetna",
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
