7587aceac4a3beeac2b5538b4da30335d46e3604
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2020-04-10 15:27:52 +0200

    obrisani nepotrebni entiteti i servisi

--- projekat/src/app/entities/rentacarProfil.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RentACarProfil } from './rentacarProfil';
-
-describe('RentACarProfil', () => {
-  it('should create an instance', () => {
-    expect(new RentACarProfil()).toBeTruthy();
-  });
-});
