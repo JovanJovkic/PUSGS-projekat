e3ae4a1417f592e13d48d257f8e4d3bb2c46a8ae
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2020-04-12 22:45:29 +0200

    vozila filter prikaz

--- projekat/src/app/components/rent-profil/rent-profil.component.ts
+++ projekat/src/app/components/rent-profil/rent-profil.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { RentACarService } from 'src/app/services/rent-a-car.service';
 import { RentACarServis } from 'src/app/entities/rentacar';
 import { ActivatedRoute } from '@angular/router';
+import { Vozilo } from 'src/app/entities/vozilo';
+import { AbstractFilterParam } from 'src/app/entities/abstract-filter-param/abstract-filter-param';
+import { StringFilterParam } from 'src/app/entities/string-filter-param/string-filter-param';
+import { NumberFilterParam } from 'src/app/entities/number-filter-param/number-filter-param';
 
 @Component({
   selector: 'app-rent-profil',
@@ -13,6 +17,9 @@ export class RentProfilComponent implements OnInit {
   allProfiles: Array<RentACarServis>;
   profil:RentACarServis;
   id: number;
+
+  allVozila:Array<Vozilo>;
+  filteredVozila:Array<Vozilo>;
   
   constructor(private rentACarServis: RentACarService,private route: ActivatedRoute) { 
     this.allProfiles = new Array<RentACarServis>();
@@ -29,8 +36,81 @@ export class RentProfilComponent implements OnInit {
       if(element.id==this.id)
       {
         this.profil=element;
+        this.allVozila = this.profil.vozila;
+        this.filteredVozila = this.allVozila;
       }
     });
+  }
+
+  filterVozila(): void {
+    let filterParams = new Array<AbstractFilterParam>();
+    if (this.getFilterFieldValue("MestoPreFilter")) {
+      filterParams.push(this.addMestoPreFilterParam());
+    }
+    if (this.getFilterFieldValue("DatumPreFilter")) {
+      filterParams.push(this.addDatumPreFilterParam());
+    }
+    if (this.getFilterFieldValue("MestoVraFilter")) {
+      filterParams.push(this.addMestoVraFilterParam());
+    }
+    if (this.getFilterFieldValue("DatumVraFilter")) {
+      filterParams.push(this.addDatumVraFilterParam());
+    }
+    if (this.getFilterFieldValue("TipVozilaFilter")) {
+      filterParams.push(this.addTipVozilaFilterParam());
+    }
+    if (this.getFilterFieldValue("BrojPutnikaFilter")) {
+      filterParams.push(this.addBrojPutnikaFilterParam());
+    }
+    if (this.getFilterFieldValue("CenaOdFilter")) {
+      filterParams.push(this.addCenaOdFilterParam());
+    }
+    if (this.getFilterFieldValue("CenaDoFilter")) {
+      filterParams.push(this.addCenaDoFilterParam());
+    }
+
+    //this.filterVozila = this.rentACarService.filterRentCarServise(this.allRentACarServis, filterParams);
+  }
+
+
+  getFilterFieldValue(filterFieldId: string) {
+    return (<HTMLInputElement> document.getElementById(filterFieldId)).value;
+  }
+
+  addMestoPreFilterParam(): ReturnType<any> {
+    return new StringFilterParam("MestoPreFilter", this.getFilterFieldValue("MestoPreFilter"));
+  }
+
+  addDatumPreFilterParam(): ReturnType<any> {
+    return new NumberFilterParam("DatumPreFilter", +this.getFilterFieldValue("DatumPreFilter"));
+  }
+
+  addMestoVraFilterParam(): ReturnType<any> {
+    return new StringFilterParam("MestoVraFilter", this.getFilterFieldValue("MestoVraFilter"));
+  }
+
+  addDatumVraFilterParam(): ReturnType<any> {
+    return new NumberFilterParam("DatumVraFilter", +this.getFilterFieldValue("DatumVraFilter"));
+  }
+
+  addTipVozilaFilterParam(): ReturnType<any> {
+    return new StringFilterParam("TipVozilaFilter", this.getFilterFieldValue("TipVozilaFilter"));
+  }
+
+  addBrojPutnikaFilterParam(): ReturnType<any> {
+    return new NumberFilterParam("BrojPutnikaFilter", +this.getFilterFieldValue("BrojPutnikaFilter"));
+  }
+
+  addCenaOdFilterParam(): ReturnType<any> {
+    return new StringFilterParam("CenaOdFilter", this.getFilterFieldValue("CenaOdFilter"));
+  }
+
+  addCenaDoFilterParam(): ReturnType<any> {
+    return new NumberFilterParam("CenaDoFilter", +this.getFilterFieldValue("CenaDoFilter"));
+  }
+
+  resetFilter(): void {
+    this.filteredVozila = this.allVozila;
   }
 
 }
