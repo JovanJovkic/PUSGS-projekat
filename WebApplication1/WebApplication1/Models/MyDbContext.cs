705a04bd3440aa93cfca2c3538421a4345ccd286
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2020-05-11 21:45:25 +0200

    Entity Framework

---
+++ WebApplication1/WebApplication1/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
